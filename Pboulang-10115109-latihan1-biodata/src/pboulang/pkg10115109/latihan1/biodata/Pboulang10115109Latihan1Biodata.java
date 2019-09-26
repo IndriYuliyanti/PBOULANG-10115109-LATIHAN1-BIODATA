eab80ee4be7d21eb1262a7a701fdcd1ce5606d6b
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-09-26 21:06:36 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan1-biodata/src/pboulang/pkg10115109/latihan1/biodata/Pboulang10115109Latihan1Biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan1.biodata;
+
+/**
+ *
+ * @author User
+ */
+public class Pboulang10115109Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA INDRI " + "\n" + "----------");
+        System.out.println("Nama \t\t: Indri yuliyanti");
+        System.out.println("Alamat \t\t: jl mohamad toha ");
+        System.out.println("tempat lahir \t: majalengka");
+        System.out.println("tanggal lahir\t: 15 juli 1997");
+        System.out.println("------------------------------");
+        System.out.println("hobi \t\t: traveling");
+        System.out.println("makanan fav \t\t: apa aja yang pnting enak ");
+        System.out.println("minuman fav\t\t: thaitea");
+    }
+    
+}
